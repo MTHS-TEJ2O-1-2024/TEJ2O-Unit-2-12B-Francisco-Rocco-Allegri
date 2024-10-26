32a83d1392ba3e8a6e4706998d311208dc5d921e
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-10-25 21:40:15 -0400

    code well finished

--- main.ts
+++ main.ts
@@ -6,45 +6,50 @@
 */
 
 let neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-let distanceToObject: number = 0
+let distanceToObject: number = 10
+basic.showNumber(distanceToObject)
 
-basic.clearScreen()
+//For debugging purposes, I'm commenting the line below to see the assigned value of 10
+//basic.clearScreen()
 neopixelStrip.clear()
 neopixelStrip.show()
-basic.showIcon(IconNames.Happy)
 
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function () 
+{
 
     neopixelStrip.clear()
     neopixelStrip.show()
-    distanceToObject = sonar.ping(
+    distanceToObject = sonar.ping
+    (
         DigitalPin.P1,
         DigitalPin.P2,
         PingUnit.Centimeters
     )
+    basic.showNumber(distanceToObject)
 
-if ( distanceToObject < 10) {
-    
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
-    neopixelStrip.show()
-    basic.pause(2000)
-    neopixelStrip.clear()
-    neopixelStrip.show()
+    if (distanceToObject < 10) {
 
-} else {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+        basic.pause(2000)
+        neopixelStrip.clear()
+        neopixelStrip.show()
 
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.show()
-    basic.pause(2000)
-    neopixelStrip.clear()
-    neopixelStrip.show()
+    }
+    else {
 
-}
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.show()
+        basic.pause(2000)
+        neopixelStrip.clear()
+        neopixelStrip.show()
+
+    }
 
 })
